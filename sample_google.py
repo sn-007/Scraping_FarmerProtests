838f16b12ae06b543f42044c7abcf9b3fee5273a
Swamy Naidu <snpro@pop-os.localdomain>
2021-02-12 11:34:12 +0530

    sample scrap

---
+++ sample_google.py
@@ -0,0 +1,24 @@
+from requests_html import HTMLSession
+session = HTMLSession()
+url = "https://news.google.com/topics/CAAqLAgKIiZDQkFTRmdvTkwyY3ZNVEZ0ZG1kd2VISndiQklGWlc0dFIwSW9BQVAB?hl=en-IN&gl=IN&ceid=IN%3Aen"
+pointer=session.get(url)
+pointer.html.render(sleep=1, scrolldown=2)
+titles = pointer.html.find('article')
+newslist=[]
+
+for item in titles:
+    try:
+        req=item.find('h3', first=True)
+        temp={
+        'title':req.text,
+        'link':req.absolute_links
+        }
+        newslist.append(temp)
+    except:
+        pass
+
+
+for item in newslist:
+    print(item['title'])
+    print(" ")
+
